1741ecdd3de8b9783c5f6e4b705d0184a15e2b33
WyattLau <wyatt6@163.com>
2026-03-25 20:27:53 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,3 @@
-// import ElementPlus from 'element-plus'
-// import zhCN from 'element-plus/es/locale/lang/zh-cn'
-// import 'element-plus/dist/index.css'
-// ----- 以上代码无用了，因为使用了unplugin-vue-components和unplugin-auto-import后不再需要全量引入了 -----
 // 有坑：样式的引入需要放在前面
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import installDirectives from './directives' // 引入自定义指令
@@ -13,8 +9,6 @@ import { useSystemStore } from './stores/system'
 
 const app = createApp(App)
 
-// app.use(ElementPlus, { locale: zhCN })
-// ----- 以上代码无用了，因为使用了unplugin-vue-components和unplugin-auto-import后不再需要全量引入了 -----
 app.use(createPinia())
 
 // 全局注册ElementPlus提供的图标
