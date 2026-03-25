31402dc9166a321c4f8cca3a23471d739b89108e
WyattLau <wyatt6@163.com>
2026-03-25 18:17:28 +0800

    不再需要全局导入ElementPlus

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
+// import ElementPlus from 'element-plus'
+// import zhCN from 'element-plus/es/locale/lang/zh-cn'
+// import 'element-plus/dist/index.css'
+// ----- 以上代码无用了，因为使用了unplugin-vue-components和unplugin-auto-import后不再需要全量引入了 -----
 // 有坑：样式的引入需要放在前面
-import ElementPlus from 'element-plus'
-import zhCN from 'element-plus/es/locale/lang/zh-cn'
-import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import installDirectives from './directives' // 引入自定义指令
 import './style.css' // 引入全局样式（包含Tailwind样式）
@@ -12,7 +13,8 @@ import { useSystemStore } from './stores/system'
 
 const app = createApp(App)
 
-app.use(ElementPlus, { locale: zhCN })
+// app.use(ElementPlus, { locale: zhCN })
+// ----- 以上代码无用了，因为使用了unplugin-vue-components和unplugin-auto-import后不再需要全量引入了 -----
 app.use(createPinia())
 
 // 全局注册ElementPlus提供的图标
