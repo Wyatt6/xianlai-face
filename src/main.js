cc1391411210c13c6bae9820a94fd05c0d794f3d
WyattLau <wyatt6@163.com>
2026-03-25 09:02:49 +0800

    引入ElementPlus图标库并全局注册

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 import ElementPlus from 'element-plus'
 import zhCN from 'element-plus/es/locale/lang/zh-cn'
 import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import './style.css' // 引入全局样式（包含Tailwind样式）
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -13,6 +14,12 @@ const app = createApp(App)
 
 app.use(ElementPlus, { locale: zhCN })
 app.use(createPinia())
+
+// 全局注册ElementPlus提供的图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
 app.use(router)
 
 // app.mount('#app')
