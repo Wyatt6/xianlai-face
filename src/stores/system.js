9b8ca38faa163e7fb3f60d60245d25168364564c
WyattLau <wyatt6@163.com>
2026-04-03 15:19:02 +0800

    新增：初始化时解析租户信息

--- src/stores/system.js
+++ src/stores/system.js
@@ -2,8 +2,9 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import axios from 'axios'
 import { notEmpty, hasText } from '@/utils/common'
-import { useRouterStore } from '@/router'
+import { useTenantStore } from './tenant'
 import { useConfigStore } from './config'
+import { useRouterStore } from '@/router'
 import { usePathStore } from './path'
 import { useMenuStore } from './menu'
 import { useApiStore } from '@/apis'
@@ -30,53 +31,27 @@ export const useSystemStore = defineStore('system', () => {
     if (!initing.value) {
       initing.value = true
       await axios
-        .get('/api/system/common/init/getInitData', {
+        .get('/api/system/core/init/getInitData', {
           headers: { 'Content-Type': 'application/json; charset=utf-8' },
+          params: { domain: window.location.hostname },
           timeout: 60000
         })
         .then(async response => {
           const result = response.data
           if (result.success) {
-            if (notEmpty(result.data) && notEmpty(result.data.checksum)) {
+            if (notEmpty(result.data)) {
+              console.log(result)
               initData.value = result.data
-              // 清除旧的路由实例
-              await useRouterStore().clearRouter()
-              // 系统参数
-              if (notEmpty(result.data.systemOptions) && hasText(result.data.checksum.systemOptionsChecksum)) {
-                await useOptionStore().evalData('SYSTEM', result.data.systemOptions, result.data.checksum.systemOptionsChecksum)
-                console.log('系统参数数据加载完成')
+              // 租户信息
+              if (notEmpty(result.data.tenant)) {
+                useTenantStore().evalData(result.data.tenant)
+                console.log('租户信息加载完成')
               }
-              // 系统路径
-              if (notEmpty(result.data.paths) && hasText(result.data.checksum.pathsChecksum)) {
-                await usePathStore().evalData(result.data.paths, result.data.checksum.pathsChecksum)
-                console.log('路径数据加载完成')
-              }
-              // 系统路由
-              if (notEmpty(result.data.routes) && hasText(result.data.checksum.routesChecksum)) {
-                await useRouterStore().evalData(result.data.routes, result.data.checksum.routesChecksum)
-                console.log('路由数据加载完成')
-              }
-              // 系统菜单
-              if (notEmpty(result.data.menus) && hasText(result.data.checksum.menusChecksum)) {
-                await useMenuStore().evalData(result.data.menus, result.data.checksum.menusChecksum)
-                console.log('菜单数据加载完成')
-              }
-              // 注册router插件
-              if (app != null) {
-                app.use(useRouterStore().getRouter())
-                console.log('router插件注册完成')
-              }
-              // 系统接口（要用到router插件）
-              if (notEmpty(result.data.apis) && hasText(result.data.checksum.apisChecksum)) {
-                await useApiStore().evalData(result.data.apis, result.data.checksum.apisChecksum)
-                console.log('接口数据加载完成')
-              }
-              // 挂载页面到Vue应用
-              console.log('系统初始化完成')
-              if (app != null) {
-                app.mount('#app')
-                console.log('#app已挂载')
-              }
+              // // 配置数据
+              // if (notEmpty(result.data.configs)) {
+              //   useConfigStore().evalData('SYSTEM', result.data.systemOptions, result.data.checksum.systemOptionsChecksum)
+              //   console.log('配置数据加载完成')
+              // }
             }
           } else {
             initFail()
@@ -90,6 +65,51 @@ export const useSystemStore = defineStore('system', () => {
         .finally(() => {
           initing.value = false
         })
+
+      // await axios
+      //   .get('/api/system/common/init/getInitData', {
+      //     headers: { 'Content-Type': 'application/json; charset=utf-8' },
+      //     timeout: 60000
+      //   })
+      //   .then(async response => {
+      //       if (notEmpty(result.data) && notEmpty(result.data.checksum)) {
+      //         initData.value = result.data
+      //         // 清除旧的路由实例
+      //         await useRouterStore().clearRouter()
+      //         // 系统路径
+      //         if (notEmpty(result.data.paths) && hasText(result.data.checksum.pathsChecksum)) {
+      //           await usePathStore().evalData(result.data.paths, result.data.checksum.pathsChecksum)
+      //           console.log('路径数据加载完成')
+      //         }
+      //         // 系统路由
+      //         if (notEmpty(result.data.routes) && hasText(result.data.checksum.routesChecksum)) {
+      //           await useRouterStore().evalData(result.data.routes, result.data.checksum.routesChecksum)
+      //           console.log('路由数据加载完成')
+      //           console.log(useRouterStore().routes)
+      //         }
+      //         // 系统菜单
+      //         if (notEmpty(result.data.menus) && hasText(result.data.checksum.menusChecksum)) {
+      //           await useMenuStore().evalData(result.data.menus, result.data.checksum.menusChecksum)
+      //           console.log('菜单数据加载完成')
+      //         }
+      //         // 注册router插件
+      //         if (app != null) {
+      //           app.use(useRouterStore().getRouter())
+      //           console.log('router插件注册完成')
+      //         }
+      //         // 系统接口（要用到router插件）
+      //         if (notEmpty(result.data.apis) && hasText(result.data.checksum.apisChecksum)) {
+      //           await useApiStore().evalData(result.data.apis, result.data.checksum.apisChecksum)
+      //           console.log('接口数据加载完成')
+      //         }
+      //         // 挂载页面到Vue应用
+      //         console.log('系统初始化完成')
+      //         if (app != null) {
+      //           app.mount('#app')
+      //           console.log('#app已挂载')
+      //         }
+      //       }
+      //   })
     }
   }
 
---
+++ src/stores/tenant.js
@@ -0,0 +1,21 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export const useTenantStore = defineStore('tenant', () => {
+  const data = ref(null)
+  const TENANT_ID = ref(null)
+  const TENANT_CODE = ref(null)
+
+  function evalData(tenant) {
+    data.value = tenant
+    TENANT_ID.value = tenant.id
+    TENANT_CODE.value = tenant.code
+  }
+
+  return {
+    data,
+    TENANT_ID,
+    TENANT_CODE,
+    evalData
+  }
+})
